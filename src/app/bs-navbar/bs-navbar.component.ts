07684565e91e059319ab9e11a350a264425e1093
fenil patel <fenilpatel1322@gmail.com>
2020-10-14 17:46:15 +0530

    Google authentication added

--- src/app/bs-navbar/bs-navbar.component.ts
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import * as Firebase from 'firebase';
+import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'bs-navbar',
   templateUrl: './bs-navbar.component.html',
   styleUrls: ['./bs-navbar.component.css']
 })
-export class BsNavbarComponent implements OnInit {
+export class BsNavbarComponent{
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private authService: AuthService) {}
+  
+  logOut(){
+    this.authService.logOut();
   }
-
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  user$: Observable<firebase.User>;
 
-  constructor() { }
+  constructor(private afAuth: AngularFireAuth) {
+    this.user$ = this.afAuth.authState;
+  }
+
+  logIn(){
+    this.afAuth.auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider());
+  }
+
+  logOut(){
+    this.afAuth.auth.signOut().then(res => console.log("Logged Out Successfully"));
+  }
 }
